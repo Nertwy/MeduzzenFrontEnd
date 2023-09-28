9b26606f795c76a87a5cd57b0a56036df2e1e152
Nertwy <deniskabos@gmail.com>
2023-09-28 13:04:07 +0300

    added Language handle Change

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import LangSelect from "./components/LangSelect.vue";
 import ModalWindow from "./components/ModalWindow.vue"
 </script>
 
 <template>
     <ModalWindow />
+    <LangSelect :items="['en','uk']" :default-selected-item="'en'"/>
     <div>{{ $t("hello") }}</div>
     <router-view></router-view>
 </template>
---
+++ src/components/LangSelect.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts" generic="T extends Language | string">
+import { onMounted, ref } from 'vue';
+import i18n from '../i18n';
+
+type Props = {
+  items: Array<T>
+  defaultSelectedItem: T
+}
+onMounted(() => {
+  const language = localStorage.getItem("lang")
+  if (!language) {
+    localStorage.setItem("lang", 'en')
+    i18n.global.locale = "en"
+  } else {
+    i18n.global.locale = language as Language
+  }
+})
+
+
+const props = defineProps<Props>()
+const item = ref<string | null | LanguageAndFlag>(props.defaultSelectedItem)
+const onSelectItem = (e: Event) => {
+  const target = e.target as HTMLSelectElement
+  if (!target.value) return
+  item.value = target.value
+  i18n.global.locale = target.value as Language
+  localStorage.setItem('lang', target.value)
+}
+
+</script>
+<template lang="">
+    <main>
+    <nav>
+      <select v-model="selectedItems" @change="onSelectItem">
+        <option v-for="item in items" :value="item">{{ item }}</option>
+      </select>
+    </nav>
+
+    <section>
+      <h1>Selected items</h1>
+      <ul>
+        <li>{{ item }}</li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<style lang="">
+    
+</style>
